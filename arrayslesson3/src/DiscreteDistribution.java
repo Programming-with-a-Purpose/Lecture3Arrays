70994edf9649caff3a3a086f1333681ee007ad2d
EllisAC <ellisac@jncb.com>
2021-11-15 14:37:05 -0500

    Discrete Distribution Question Image

--- arrayslesson3/src/DiscreteDistribution.java
+++ arrayslesson3/src/DiscreteDistribution.java
@@ -1,11 +1,23 @@
-
-
 public class DiscreteDistribution {
 
     public static void main(String[] args) {
 
 //        Discrete distribution
 //        https://introcs.cs.princeton.edu/java/14array/DiscreteDistribution.java.html
+
+
+//        To generate a random index i with probability proportional to ai:
+//
+//        Define the cumulative sums Si=a1+a2+…+ai, with S0=0.
+//        Pick a random integer r uniformly between 0 and Sn−1.
+//        Find the unique index i between 1 and n such that Si−1≤r<Si.
+//                Geometrically, this subdivides the interval [0,Sn) into n subintervals [Si−1,Si), with the length of subinterval i proportional to ai. For example, if the discrete distribution is defined by
+//
+//        (a1,a2,a3,a4,a5,a6)=(10,10,10,10,10,50),
+//        then the cumulative sums are
+//                (S1,S2,S3,S4,S5,S6)=(10,20,30,40,50,100)
+//        and the following diagram illustrates the 6 subintervals:
+
 
 //        int m = Integer.parseInt(args[0]);
 
@@ -14,6 +26,8 @@ public class DiscreteDistribution {
 //        I don't know how many integers will be entered
 
 //        System.out.println("Command Line argument Length: " + args.length);
+
+
         int m = Integer.parseInt(args[0]);
 
         int [] arr = new int[args.length-1];
